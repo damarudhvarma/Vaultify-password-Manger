d0da93acdd38a29d018e95cb990d93f886b202d6
damarudhvarma <damarudhvarma.dandu@gmail..com>
2024-08-17 13:44:25 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Manager from './components/Manager'
+import Navbar from './components/Navbar'
+
+function App() {
+ 
+
+  return (
+    <>
+   
+    <Manager />
+
+      </>
+  )
+}
+
+export default App
